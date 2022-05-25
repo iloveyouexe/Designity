841498c3082154cdb9b2b17f562ebf2b53ca38a9
Hamilton <hhamilton@bradley.com>
2022-05-25 14:48:19 -0500

    fixed small errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { <BrowserRo></BrowserRo>uter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import landing from "./pages/landing.jsx";
-import categories from "./pages/gategories.jsx";
+import categories from "./pages/categories.jsx";
 import company from "./pages/company.jsx";
 import portfolio from "./pages/portfolio.jsx";
 import resources from "./pages/resources.jsx";
+import navbar from "./components/navbar.jsx";
+import footer from "./components/footer.jsx";
 import './App.css';
 
 const App = () => {
   return (
       <BrowserRouter>
-       <Navbar />
+       {/* <Navbar /> */}
           <Routes>
               <Route path="/" element={<landing />} />
-              <Route path="/categories" element={<catgories />} />
+              <Route path="/categories" element={<categories />} />
               <Route path="/company" element={<company />} />
               <Route path="/portfolio" element={<portfolio />} />
               <Route path="/resources" element={<resources />} />
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Landing = () => {
+const landing = () => {
   return <></>;
 };
 
